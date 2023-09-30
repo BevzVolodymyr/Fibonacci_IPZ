f1d5e5541fd39667d71ae32699212c70c3abe371
BevzVolodymyr <bevz.volodymyr23@gmail.com>
2023-09-30 19:14:49 +0300

    first

---
+++ Fibonacci_IPZ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.Write("Введіть номер числа Фібоначчі: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("Номер повинен бути не менше 0.");
+        }
+        else
+        {
+            long result = Fibonacci(n);
+            Console.WriteLine($"Число Фібоначчі {n} = {result}");
+        }
+    }
+
+    static long Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
